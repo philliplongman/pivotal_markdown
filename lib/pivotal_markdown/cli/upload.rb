ba80f38b30860f52a12a9bd0dfb2dde22ad3c7d8
Phillip Longman <himself@philliplongman.com>
2016-10-09 17:24:10 -0400

    Hide code that's not working yet to merge with master.

--- lib/pivotal_markdown/cli/upload.rb
+++ lib/pivotal_markdown/cli/upload.rb
@@ -1,14 +1,14 @@
-module PivotalMarkdown
-  module CLI
-    class Base < Thor
-      include Shared
-
-      desc "upload FILE", "Parse Markdown file into individual stories and upload stories to specified project"
-      def stories(file)
-        raise non_markdown_file_error unless file.end_with? ".md"
-        MarkdownStory.parse_file(file).each(&:upload)
-      end
-
-    end
-  end
-end
+# module PivotalMarkdown
+#   module CLI
+#     class Base < Thor
+#       include Shared
+#
+#       desc "upload FILE", "Parse Markdown file into individual stories and upload stories to specified project"
+#       def stories(file)
+#         raise non_markdown_file_error unless file.end_with? ".md"
+#         MarkdownStory.parse_file(file).each(&:upload)
+#       end
+#
+#     end
+#   end
+# end
