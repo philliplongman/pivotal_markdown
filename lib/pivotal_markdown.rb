b9c69115707e55fe5762ab747706d11110032479
Phillip Longman <himself@philliplongman.com>
2016-10-14 00:37:41 -0400

    Remove Verifier wrapper & interact with ApiTracker objects directly.

--- lib/pivotal_markdown.rb
+++ lib/pivotal_markdown.rb
@@ -2,6 +2,5 @@ require "pivotal_markdown/version"
 require "pivotal_markdown/cli"
 require "pivotal_markdown/config"
 require "pivotal_markdown/error"
-require "pivotal_markdown/verifier"
 
 require "pry"
--- lib/pivotal_markdown/cli/project.rb
+++ lib/pivotal_markdown/cli/project.rb
@@ -5,28 +5,46 @@ module PivotalMarkdown
     class Project < Thor
 
       desc "set PROJECT_ID", "Set default project"
-      def set(project_id)
+      def set(id)
         raise Error.no_api_token unless config.api_token
-
-        verifier = Verifier.new(config.api_token, project_id)
-        puts "Default project set to (#{verifier.project_id}) #{verifier.project_name}."
-        config.default_project = project_id
-        config.save
+        begin
+          project = client.project(id)
+          config.default_project = id
+          config.save
+          puts "Default project set to (#{project.id}) #{project.name}."
+        rescue => error
+          error_handler error
+        end
       end
 
       desc "check", "Check configured default project"
       def check
         raise Error.no_api_token unless config.api_token
         raise Error.no_default_project unless config.default_project
-
-        verifier = Verifier.new(config.api_token, config.default_project)
-        puts "Default project set to (#{verifier.project_id}) #{verifier.project_name}."
+        begin
+          project = client.project(config.default_project)
+          puts "Default project set to (#{project.id}) #{project.name}."
+        rescue => error
+          error_handler error
+        end
       end
 
       private
 
+      def client
+        TrackerApi::Client.new(token: config.api_token)
+      end
+
       def config
         @config ||= Config.new
+      end
+
+      def error_handler(error)
+        if error.response[:body]["code"] == "invalid_authentication"
+          raise Error.invalid_api_token
+        else
+          raise Error.invalid_project_id
+        end
       end
 
     end
