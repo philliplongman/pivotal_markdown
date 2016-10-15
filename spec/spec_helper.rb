c7ca435ea22779974410c2c325f8c5bbd2e096d4
Phillip Longman <himself@philliplongman.com>
2016-10-15 01:28:38 -0400

    Mocking fits on one line now, so eliminate spec helper support file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,9 @@ require 'pivotal_markdown'
 
 require_relative 'support/config_cleaner'
 require_relative 'support/fraudulent_objects'
-require_relative 'support/spec_helper'
 
 include ConfigCleaner
 include FraudulentObjects
-include SpecHelper
 
 require 'coveralls'
 Coveralls.wear!
