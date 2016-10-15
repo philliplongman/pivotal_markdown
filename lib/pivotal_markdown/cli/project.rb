16a9ca7218aac7539d841c2330efcf3973cb52fc
Phillip Longman <himself@philliplongman.com>
2016-10-15 01:34:14 -0400

    Eliminate anti-pattern of raising errors as a means of flow control.

--- lib/pivotal_markdown/cli/project.rb
+++ lib/pivotal_markdown/cli/project.rb
@@ -3,48 +3,36 @@ require "tracker_api"
 module PivotalMarkdown
   module CLI
     class Project < Thor
+      include Shared
 
       desc "set PROJECT_ID", "Set default project"
       def set(id)
-        raise Error.no_api_token unless config.api_token
-        begin
-          project = client.project(id)
+        if config.api_token
+          project = client(config.api_token).project(id)
           config.default_project = id
           config.save
           puts "Default project set to (#{project.id}) #{project.name}."
-        rescue => error
-          error_handler error
+        else
+          puts Message.no_api_token
         end
+      rescue => error
+        puts error_message error
       end
 
       desc "check", "Check configured default project"
       def check
-        raise Error.no_api_token unless config.api_token
-        raise Error.no_default_project unless config.default_project
-        begin
-          project = client.project(config.default_project)
+        if config.api_token && config.default_project
+          project = client(config.api_token).project(config.default_project)
           puts "Default project set to (#{project.id}) #{project.name}."
-        rescue => error
-          error_handler error
+
+        elsif config.api_token.nil?
+          puts Message.no_api_token
+
+        elsif config.default_project.nil?
+          puts Message.no_default_project
         end
-      end
-
-      private
-
-      def client
-        TrackerApi::Client.new(token: config.api_token)
-      end
-
-      def config
-        @config ||= Config.new
-      end
-
-      def error_handler(error)
-        if error.response[:body]["code"] == "invalid_authentication"
-          raise Error.invalid_api_token
-        else
-          raise Error.invalid_project_id
-        end
+      rescue => error
+        puts error_message error
       end
 
     end
--- lib/pivotal_markdown/cli/token.rb
+++ lib/pivotal_markdown/cli/token.rb
@@ -3,38 +3,28 @@ require "tracker_api"
 module PivotalMarkdown
   module CLI
     class Token < Thor
+      include Shared
 
       desc "set TOKEN", "Set API token to access"
       def set(token)
-        begin
-          user = client(token).me
-          config.api_token = token
-          config.save
-          puts "Token set for #{user.name} - #{user.email}."
-        rescue
-          raise Error.invalid_api_token
-        end
+        user = client(token).me
+        config.api_token = token
+        config.save
+        puts "Token set for #{user.name} - #{user.email}."
+      rescue => error
+        puts error_message error
       end
 
       desc "check", "Check configured API token"
       def check
-        raise Error.no_api_token unless config.api_token
-        begin
+        if config.api_token
           user = client(config.api_token).me
           puts "Token set for #{user.name} - #{user.email}."
-        rescue
-          raise Error.invalid_api_token
+        else
+          puts Message.no_api_token
         end
-      end
-
-      private
-
-      def client(token)
-        TrackerApi::Client.new(token: token)
-      end
-
-      def config
-        @config ||= Config.new
+      rescue => error
+        puts error_message error
       end
 
     end
