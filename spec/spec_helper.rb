99d4c1bdfa7b5141f925b2ab2617c1d4aba2f184
Phillip Longman <himself@philliplongman.com>
2016-10-15 02:38:34 -0400

    Add #stfu method to surpress stdout during rake.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,20 @@ require 'pivotal_markdown'
 
 require_relative 'support/config_cleaner'
 require_relative 'support/fraudulent_objects'
+require_relative 'support/stfu'
 
 include ConfigCleaner
 include FraudulentObjects
+include STFU
 
 require 'coveralls'
 Coveralls.wear!
+
+
+RSpec.configure do |config|
+
+  config.around :each do |example|
+    stfu { example.run }
+  end
+
+end
