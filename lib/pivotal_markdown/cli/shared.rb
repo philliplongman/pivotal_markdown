d39d7c128a4b6f434b866c4088f7bd1e06985dab
Phillip Longman <himself@philliplongman.com>
2017-04-19 13:45:12 -0400

    Remove redundant method.

--- lib/pivotal_markdown/cli/shared.rb
+++ lib/pivotal_markdown/cli/shared.rb
@@ -15,15 +15,6 @@ module PivotalMarkdown
       end
 
       def error_message(error)
-        return "No response. Check internet connection.\n\n" if error.response.nil?
-        binding.pry
-        case error.class.to_s
-        when "RestClient::Response" then error.response.body  + "\n\n"
-        when "TrackerApi::Error"    then error.response[:body]["error"] + "\n\n"
-        end
-      end
-
-      def error_message(error)
         response = error.response
         if response.nil?
           "No response. Check internet connection.\n\n"
